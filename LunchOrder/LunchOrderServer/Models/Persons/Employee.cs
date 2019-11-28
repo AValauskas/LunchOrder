fa4c78d357e76e62846d63451a6faf09e9bf3a9c
Alauskas <37072295+Alauskas@users.noreply.github.com>
2019-11-28 16:57:28 +0200

    let's begin

---
+++ LunchOrder/LunchOrderServer/Models/Persons/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LunchOrderServer.Models.Persons
+{
+    public class Employee
+    {
+        public string Pame;
+        public string Username;
+        public string Password;
+        public string CurrentLocation;
+        public string JobLocation;
+
+            
+
+
+    }
+}
